b3ed4a56c7c5e12f5ce6960ba580ed08e934abc4
Joey Ballentine <joeyjballentine@gmail.com>
2026-08-03 23:00:41 -0500

    MSL: fold rand's update into the return expression
    85.00% -> 97.50%. The remaining delta is register allocation.

--- src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/rand.c
+++ src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/rand.c
@@ -4,8 +4,7 @@ static u32 next = 1;
 
 int rand()
 {
-    next = next * 1103515245 + 12345;
-    return ((next >> 16) & 0x7fff);
+    return (((next = next * 1103515245 + 12345) >> 16) & 0x7fff);
 }
 
 void srand(u32 seed)
