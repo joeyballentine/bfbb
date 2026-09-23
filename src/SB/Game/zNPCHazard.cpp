d1f91d4433d417e4a8e6d33f1ba348dde6636afa
Joey Ballentine <joeyjballentine@gmail.com>
2026-09-22 20:43:46 -0500

    fix(decomp): Return GrabModel's result through a conditional

--- src/SB/Game/zNPCHazard.cpp
+++ src/SB/Game/zNPCHazard.cpp
@@ -1042,7 +1042,7 @@ S32 NPCHazard::GrabModel(en_hazmodel idx_model)
         }
     }
 
-    return this->mdl_hazard != NULL;
+    return this->mdl_hazard ? 1 : 0;
 }
 
 void NPCHazard::FreeModel()
